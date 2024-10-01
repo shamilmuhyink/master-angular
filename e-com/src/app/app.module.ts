7fc6960a64329c64bf25868a65dd4bb049e9a244
shamilmuhyink <shamilmuhyink@gmail.com>
2024-10-01 21:39:13 +0530

    "Updated app component HTML, added RegisterComponent and HeaderComponent to app module, and modified index.html title"

--- e-com/src/app/app.module.ts
+++ e-com/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegisterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
